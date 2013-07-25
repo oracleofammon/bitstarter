6020b624c18a6f6d56bdc88b921e75808e5f7f66
Yazan Damiri <ydamiri@hotmail.com>
2013-07-25 19:34:15 +0000

    one more time

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send((fs.writeFile(index.html));
+  response.send((fs.readFileSync('index.html'));
+  buffer.toString('utf-8'); 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
